6a1bd6e821db206bc6cb7bda525d61bde5a3b7b6
Ali Mammadov <ali.mammadov@easlearn.com>
2023-10-03 21:39:56 +0400

    .

--- WinForms Lesson 1/WinForms Lesson 1/Form1.Designer.cs
+++ WinForms Lesson 1/WinForms Lesson 1/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            BakuBtn = new Button();
+            PicOfCurrCity = new PictureBox();
+            LondonBtn = new Button();
+            TimeTextbox = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)PicOfCurrCity).BeginInit();
+            SuspendLayout();
+            // 
+            // BakuBtn
+            // 
+            BakuBtn.AccessibleName = "";
+            BakuBtn.Location = new Point(81, 11);
+            BakuBtn.Name = "BakuBtn";
+            BakuBtn.Size = new Size(64, 25);
+            BakuBtn.TabIndex = 0;
+            BakuBtn.Tag = "";
+            BakuBtn.Text = "Baku";
+            BakuBtn.UseVisualStyleBackColor = true;
+            BakuBtn.Click += button1_Click;
+            // 
+            // PicOfCurrCity
+            // 
+            PicOfCurrCity.Location = new Point(12, 60);
+            PicOfCurrCity.Name = "PicOfCurrCity";
+            PicOfCurrCity.Size = new Size(760, 378);
+            PicOfCurrCity.SizeMode = PictureBoxSizeMode.StretchImage;
+            PicOfCurrCity.TabIndex = 1;
+            PicOfCurrCity.TabStop = false;
+            PicOfCurrCity.Click += pictureBox1_Click;
+            // 
+            // LondonBtn
+            // 
+            LondonBtn.Location = new Point(0, 12);
+            LondonBtn.Name = "LondonBtn";
+            LondonBtn.Size = new Size(75, 23);
+            LondonBtn.TabIndex = 2;
+            LondonBtn.Text = "London";
+            LondonBtn.UseVisualStyleBackColor = true;
+            LondonBtn.Click += button2_Click;
+            // 
+            // TimeTextbox
+            // 
+            TimeTextbox.Location = new Point(244, 29);
+            TimeTextbox.Name = "TimeTextbox";
+            TimeTextbox.Size = new Size(288, 23);
+            TimeTextbox.TabIndex = 3;
+            TimeTextbox.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(TimeTextbox);
+            Controls.Add(LondonBtn);
+            Controls.Add(PicOfCurrCity);
+            Controls.Add(BakuBtn);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)PicOfCurrCity).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button BakuBtn;
+        private PictureBox PicOfCurrCity;
+        private Button LondonBtn;
+        private TextBox TimeTextbox;
     }
 }
--- WinForms Lesson 1/WinForms Lesson 1/Form1.cs
+++ WinForms Lesson 1/WinForms Lesson 1/Form1.cs
@@ -1,3 +1,7 @@
+using System.Text.Json;
+using System.Windows.Forms;
+using WinForms_Lesson_1.Properties;
+
 namespace WinForms_Lesson_1
 {
     public partial class Form1 : Form
@@ -5,6 +9,89 @@ namespace WinForms_Lesson_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button asdf = new Button();
+            asdf.Text = ((Form1)sender).Text;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Create an instance of HttpClient
+                using (HttpClient client = new HttpClient())
+                {
+                    string apiUrl = "http://worldtimeapi.org/api/timezone/Asia/Baku";
+
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+
+                        MyTimeZone data = JsonSerializer.Deserialize<MyTimeZone>(content);
+                        string dateTimeString = data.datetime;
+                        DateTime dateTime = DateTime.Parse(dateTimeString);
+
+                        TimeTextbox.Text = dateTime.ToString("HH:mm:ss");
+
+                        Image newImage = Image.FromFile("C:/Users/mamma_er07/Downloads/baku.jpg");
+                        PicOfCurrCity.Image = newImage;
+                    }
+                    else
+                    {
+                        TimeTextbox.Text = $"API request failed with status code {response.StatusCode}";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TimeTextbox.Text = $"An error occurred: {ex.Message}";
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string apiUrl = "http://worldtimeapi.org/api/timezone/Europe/London";
+
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+
+                        MyTimeZone data = JsonSerializer.Deserialize<MyTimeZone>(content);
+                        string dateTimeString = data.datetime;
+                        DateTime dateTime = DateTime.Parse(dateTimeString);
+
+                        TimeTextbox.Text = dateTime.ToString("HH:mm:ss");
+
+                        Image newImage = Image.FromFile("C:/Users/mamma_er07/Downloads/london.jpg");
+                        PicOfCurrCity.Image = newImage;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TimeTextbox.Text = $"An error occurred: {ex.Message}";
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
